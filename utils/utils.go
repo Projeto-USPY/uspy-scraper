e5bad395076f2d50c926188325255446fea4985d
Thiago <thiagop@usp.br>
2021-05-15 22:36:41 -0300

    update utils

--- utils/utils.go
+++ utils/utils.go
@@ -8,10 +8,19 @@ import (
 	"golang.org/x/net/html/charset"
 )
 
-// Returns HTTP response and io.Reader from http.Get, which should substitute http.Body, so characters are read with UTF-8 encoding
-// Already panics if error, remember to close response.Body
-func HTTPGetWithUTF8(client *http.Client, url string) (*http.Response, io.Reader, error) {
-	resp, err := client.Get(url)
+// Returns HTTP response and io.Reader from http Request, which should substitute http.Body, so characters are read with UTF-8 encoding
+// Remember to close response.Body
+func MakeRequestWithUTF8(client *http.Client, url, method string, body io.Reader, headers map[string]string) (*http.Response, io.Reader, error) {
+	req, err := http.NewRequest(method, url, body)
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, nil, err
 	}
