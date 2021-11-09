e56d9c38815251dfdfa16c95645b9e9a22c1de11
tprei <thiagopreischadt@gmail.com>
2021-11-08 22:38:03 -0300

    add status code check in scraper

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,6 +1,7 @@
 package scraper
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 
@@ -17,21 +18,23 @@ type Scraper interface {
 	Scrape(reader io.Reader) (db.Writer, error)
 }
 
-func Start(scraper Scraper, startURL string, method string, body io.Reader, headers map[string]string) (db.Writer, error) {
+func Start(scraper Scraper, startURL string, method string, body io.Reader, headers map[string]string, infereContentType bool) (db.Writer, error) {
 	client := &http.Client{
 		Timeout: 0,
 	}
 
 	var object db.Writer
 
-	if resp, reader, err := utils.MakeRequestWithUTF8(client, startURL, method, body, headers); err != nil {
+	if resp, reader, err := utils.MakeRequestWithUTF8(client, startURL, method, body, headers, infereContentType); err != nil {
 		return nil, err
-	} else {
+	} else if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		// successfully got start page
 		defer resp.Body.Close()
 		if object, err = scraper.Scrape(reader); err != nil {
 			return nil, err
 		}
+	} else {
+		return nil, fmt.Errorf("could not start scraper: invalid url:" + startURL)
 	}
 
 	return object, nil
