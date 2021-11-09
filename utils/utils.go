b178a79d3cc9c8cd6ce64fcafdd869645ab8dfd8
tprei <thiagopreischadt@gmail.com>
2021-11-08 22:35:26 -0300

    add default headers and make inference of content type optional

--- utils/utils.go
+++ utils/utils.go
@@ -2,16 +2,26 @@
 package utils
 
 import (
+	"bufio"
 	"io"
 	"net/http"
 
 	"golang.org/x/net/html/charset"
 )
 
+var defaultHeaders = map[string]string{
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+}
+
 // Returns HTTP response and io.Reader from http Request, which should substitute http.Body, so characters are read with UTF-8 encoding
 // Remember to close response.Body
-func MakeRequestWithUTF8(client *http.Client, url, method string, body io.Reader, headers map[string]string) (*http.Response, io.Reader, error) {
+func MakeRequestWithUTF8(client *http.Client, url, method string, body io.Reader, headers map[string]string, infereContentType bool) (*http.Response, io.Reader, error) {
 	req, err := http.NewRequest(method, url, body)
+
+	for k, v := range defaultHeaders {
+		req.Header.Add(k, v)
+	}
+
 	for k, v := range headers {
 		req.Header.Add(k, v)
 	}
@@ -25,10 +35,14 @@ func MakeRequestWithUTF8(client *http.Client, url, method string, body io.Reader
 		return nil, nil, err
 	}
 
-	reader, err := charset.NewReader(resp.Body, resp.Header["Content-Type"][0])
-	if err != nil {
-		return nil, nil, err
+	if infereContentType {
+		reader, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
+		if err != nil {
+			return nil, nil, err
+		}
+
+		return resp, reader, nil
 	}
 
-	return resp, reader, nil
+	return resp, bufio.NewReader(resp.Body), nil
 }
