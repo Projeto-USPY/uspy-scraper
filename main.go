a99cf06274f8e999ef15569708d164a3f2092349
Thiago <thiagop@usp.br>
2022-01-20 22:33:56 -0300

    add log level to docker compose

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"os"
+
 	"github.com/Projeto-USPY/uspy-scraper/server"
 	log "github.com/sirupsen/logrus"
 )
 
 func init() {
-	log.SetLevel(log.DebugLevel)
+	if level, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		switch level {
+		case "info":
+			log.SetLevel(log.InfoLevel)
+		case "warn":
+			log.SetLevel(log.WarnLevel)
+		case "error":
+			log.SetLevel(log.ErrorLevel)
+		default:
+			log.SetLevel(log.DebugLevel)
+		}
+	}
 }
 
 func main() {
