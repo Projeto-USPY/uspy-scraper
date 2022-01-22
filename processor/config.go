eed9d260a3b60592f1b2d1ee75676c8992d57ff5
Thiago <thiagop@usp.br>
2022-01-21 21:04:51 -0300

    change defaults

--- processor/config.go
+++ processor/config.go
@@ -3,13 +3,13 @@ package processor
 import "github.com/kelseyhightower/envconfig"
 
 type env struct {
-	NumWorkers           int     `envconfig:"NUM_WORKERS" default:"500"`
+	NumWorkers           int     `envconfig:"NUM_WORKERS" default:"300"`
 	MaxWorkers           int     `envconfig:"MAX_WORKERS" default:"300"`
-	FractionalNumWorkers float64 `envconfig:"FRACTIONAL_NUM_WORKERS" default:"0.35"`
+	FractionalNumWorkers float64 `envconfig:"FRACTIONAL_NUM_WORKERS" default:"0.25"`
 	MaxAttempts          int     `envconfig:"MAX_ATTEMPTS" default:"3"`
 	FixedAttempts        bool    `envconfig:"FIXED_ATTEMPTS" default:"true"`
 	DelayAttempts        bool    `envconfig:"DELAY_ATTEMPTS" default:"false"`
-	Timeout              int     `envconfig:"TIMEOUT" default:"600"` // timeout in seconds
+	Timeout              int     `envconfig:"TIMEOUT" default:"-1"` // timeout in seconds, -1 is unlimited
 }
 
 var Config env
